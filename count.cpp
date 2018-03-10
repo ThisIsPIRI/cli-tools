a75bfac2d847cfeb23d7e6c44849a60f2e04b7d0
busyVision <>
2018-03-10 18:19:53 +0900

    fail

--- count.cpp
+++ count.cpp
@@ -1,27 +1,44 @@
-/*TODO: import standard libraries via modules
+/*I'm giving unicode/international strings support up for an indefinite amount of time. Contributions welcome.
+
+TODO: import standard libraries via modules
 TODO: the C++ module system is not complete yet. Refactor to conform to the standars
 when it comes out*/
+#define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
+#include <codecvt>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <vector>
 
 export module count; //module count; doesn't work but this does.
 
 using namespace std;
 
+#ifdef _WIN32
+typedef wchar_t onechar;
+typedef wstring onestring;
+typedef wifstream oneifstream;
+typedef wstringstream onestringstream;
+#else
+typedef char onechar;
+typedef string onestring;
+typedef ifstream oneifstream;
+typedef stringstream onestringstream;
+#endif
+
 export class Paragraph {
 public:
 	int chars, words, sentences;
-	string firstWord, lastWord;
-	Paragraph(int c, int w, int s, string f, string l) : chars(c), words(w), sentences(s), firstWord(f), lastWord(l) {}
+	onestring firstWord, lastWord;
+	Paragraph(int c, int w, int s, onestring f, onestring l) : chars(c), words(w), sentences(s), firstWord(f), lastWord(l) {}
 };
 
 export class Counter {
 protected:
-	char read;
+	onechar read;
 	bool inWord, inBlank, sentenceEndMet, paragraphJustEnded;
 	int lineTemp, spaceTemp;
-	string lastWord, firstInThis;
+	onestring lastWord, firstInThis;
 	bool isAlphabet(char c) {
 		return '!' <= c && c <= '~';
 	}
@@ -47,10 +64,15 @@ public:
 		lineTemp = spaceTemp = charCount = wordCount = sentenceCount = paraCount = paraCharCount = paraWordCount = paraSentenceCount = 0;
 		paragraphs.clear();
 	}
-	void count(ifstream& file) {
+	void count(oneifstream& file) {
 		initialize();
-		while (file.good()) {
-			read = file.get(); //TODO: handle non-ASCII characters correctly
+		//https://stackoverflow.com/a/10607146
+		file.imbue(locale(locale::empty(), new codecvt_utf8<wchar_t>));
+		onestringstream stream;
+		stream << file.rdbuf();
+		onestring str = stream.str();
+		for(onechar& read : str) {
+			//read = file.get(); //TODO: handle non-ASCII characters correctly
 			switch (read) {
 			case '\n':
 				lineTemp++;
